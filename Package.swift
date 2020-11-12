be55021168943cabab079b8f948b7256b90d0d70
Bruno Basas <bruno.basas@apiumhub.com>
2020-11-12 11:57:17 +0100

    Modified xcframework

--- Package.swift
+++ Package.swift
@@ -10,12 +10,12 @@ let package = Package(
             name: "TestSDKFramework",
             targets: ["TestSDKFramework"])
     ],
-    dependencies: [
-        .package(
-            url: "https://github.com/facebook/facebook-ios-sdk",
-            from: "8.1.0"
-        )
-    ],
+//    dependencies: [
+//        .package(
+//            url: "https://github.com/facebook/facebook-ios-sdk",
+//            from: "8.1.0"
+//        )
+//    ],
     targets: [
         .binaryTarget(
             name: "TestSDKFramework",
