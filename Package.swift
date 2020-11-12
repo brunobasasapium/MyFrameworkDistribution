3cd1deb9c42e5ea06df5bf01996bcc1e2908156b
Bruno Basas <bruno.basas@apiumhub.com>
2020-11-12 11:31:06 +0100

    Added facebook dependencie into the package

--- Package.swift
+++ Package.swift
@@ -3,15 +3,22 @@ import PackageDescription
 let package = Package(
     name: "TestSDKFramework",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v13)
     ],
     products: [
         .library(
             name: "TestSDKFramework",
             targets: ["TestSDKFramework"])
     ],
+    dependencies: [
+        .package(
+            url: "https://github.com/facebook/facebook-ios-sdk",
+            from: "8.1.0"
+        )
+    ],
     targets: [
         .binaryTarget(
             name: "TestSDKFramework",
             path: "TestSDKFramework.xcframework")
     ])
+
